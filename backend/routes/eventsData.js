77f25741d5ee36e24e02aaf3e916e555e6ce3b2b
lfleonse <luiss.leon98@gmail.com>
2022-10-06 00:14:30 -0500

    Fixed message sent on put event

--- backend/routes/eventsData.js
+++ backend/routes/eventsData.js
@@ -134,7 +134,7 @@ router.put("/:id", (req, res, next) => {
             if (error) {
                 return next(error);
             } else {
-                res.json(data);
+                res.send('event updated');
             }
         }
     );
